f2fb02fbdbb78f63e05d9fbdae09bb448f1bd0ad
Alex Parvis <me@Mes-MacBook-Pro.local>
2020-05-13 12:52:50 +0100

    spelling error

--- src/utils/api/tankPost.js
+++ src/utils/api/tankPost.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const tankPost = (data) => {
-  //example api to post fish adata to, just returns back the JSON that was sent
+  //example api to post fish data to, just returns back the JSON that was sent, can be viewed i the console.
   axios.post(`https://jsonplaceholder.typicode.com/users`, { data })
     .then(res => {
       console.log(res);
